45bee62c9c1553af4eb1e8b97528b2c5fbe50511
Osi ic <osiic.offcl@gmail.com>
2024-04-21 00:51:08 +0800

    setup(metadata) 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,12 +10,32 @@ export const viewport: Viewport = {
 };
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://acme.com'),
   title: "Mabica",
   description:
     'Mabica (Mari Bikin Cerita) adalah perkumpulan orang gabut yang mungkin membuat cerita supaya tidak "rin udah makan" atau "sehat?" saat bermain discord.',
-   
+  openGraph: {
+    title: "Mabica",
+    description:
+      'Mabica (Mari Bikin Cerita) adalah perkumpulan orang gabut yang mungkin membuat cerita supaya tidak "rin udah makan" atau "sehat?" saat bermain discord.',
+    siteName: "Mabica",
+    images: [
+      {
+        url: "/image/og.webp", // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "/image/og-alt.webp", // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: "Image OG alt",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
-
 
 export default function RootLayout({
   children,
