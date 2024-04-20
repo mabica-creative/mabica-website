7a4fc2225221218d1439e650277d53fe42da449b
Osi ic <osiic.offcl@gmail.com>
2024-04-20 23:36:07 +0800

    setup(color, database)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        text: "#ebe9fc",
+        background: "#010104",
+        primary: "#3a31d8",
+        secondary: "#020024",
+        accent: "#0600c2",
       },
     },
   },
-  plugins: [],
 };
 export default config;
