35a88a8aa0ddf3155d1fdc4637333ff32ed080b3
Osi ic <osiic.offcl@gmail.com>
2024-04-25 05:42:59 +0800

    jashbdajsvdyusdvfkavhs

--- src/app/(public)/team/page.tsx
+++ src/app/(public)/team/page.tsx
@@ -1,16 +1,20 @@
 import { getTeam } from "@/frontend/team";
 import { team } from "@prisma/client";
-import { notFound } from 'next/navigation'
+import { notFound } from "next/navigation";
 
 export default async function TeamPage() {
   const data = await getTeam();
   // console.log(data);
 
-  if(data?.status !== 'success') {
-    return notFound()
+  if (data?.status !== "success") {
+    return (
+      <main className="flex justify-center gap-2 items-center">
+        ada yang salah
+      </main>
+    );
   }
 
-  const team = data?.data
+  const team = data?.data;
 
   return (
     <main className="flex justify-center gap-2 items-center">
