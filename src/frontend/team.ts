50d5f8b20eae6186ff2e61c7fa2eb9915b3a1655
Osi ic <osiic.offcl@gmail.com>
2024-04-25 05:55:04 +0800

    akshdbh

--- src/frontend/team.ts
+++ src/frontend/team.ts
@@ -1,5 +1,9 @@
 export async function getTeam() {
-  const req = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/team`)
-   
-  return req.json()
+  const req = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/team`);
+
+  if (req.status !== 200) {
+    return { status: "error" };
+  }
+
+  return req.json();
 }
